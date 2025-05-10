cc6931d9b1b10fe1c8bcbabb978ed1d9e32afa54
Lect. Ali Cetinkaya (MSc.) <35449105+acetinkaya@users.noreply.github.com>
2025-05-10 11:32:07 +0300

    Add files via upload

---
+++ _1_MerhabaHayat.c
@@ -0,0 +1,19 @@
+/*
+
+C Programlama Dili Rehberi  
+Ders 1. C Programlama Dili Genel Yapýsý ve DevC++ Programýnýn Genel Yapýsý
+
+Github Kaynagý:       
+https://github.com/acetinkaya/C-Programlama-Rehberi
+
+Öðr. Gör. Ali ÇETÝNKAYA - 2025
+
+*/
+
+#include <stdio.h>
+int main() 
+{
+    printf("Merhaba Hayat");
+}
+
+
